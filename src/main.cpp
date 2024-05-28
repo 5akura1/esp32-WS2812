21835431c2331b640dc4ce1456bf9e0c0f7f2107
Sakura1 <im5akura1.w@gmail.com>
2024-05-29 05:20:09 +0800

    AP配网

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,13 @@
 #include <WiFi.h>
 #include <WebServer.h>
 #include <pgmspace.h>
+
+#include "WiFiUser.h"
+ 
+const int resetPin = 0;                    //设置重置按键引脚,用于删除WiFi信息
+int connectTimeOut_s = 15;                 //WiFi连接超时时间，单位秒
+
+
 #define WEB_SERVER WebServer
 #define ESP_RESET ESP.restart()
 
@@ -224,8 +231,8 @@ function submitVal(name, val) {
 }
 )=====";
 
-#define WIFI_SSID "5akura1"
-#define WIFI_PASSWORD "zhaoyonghao0429"
+// #define WIFI_SSID "5akura1"
+// #define WIFI_PASSWORD "zhaoyonghao0429"
 
 //#define STATIC_IP                       // uncomment for static IP, set IP below
 #ifdef STATIC_IP
@@ -238,7 +245,7 @@ function submitVal(name, val) {
 #define min(a,b) ((a)<(b)?(a):(b))
 #define max(a,b) ((a)>(b)?(a):(b))
 
-#define LED_PIN 12                       // 0 = GPIO0, 2=GPIO2
+#define LED_PIN 13                       // 0 = GPIO0, 2=GPIO2
 #define LED_COUNT 7
 
 #define WIFI_TIMEOUT 30000              // checks WiFi every ...ms. Reset after this time, if WiFi cannot reconnect.
@@ -251,7 +258,7 @@ uint8_t myModes[] = {}; // *** optionally create a custom list of effect/mode nu
 bool auto_cycle = false;
 
 WS2812FX ws2812fx = WS2812FX(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
-WEB_SERVER server(HTTP_PORT);
+WEB_SERVER server1(HTTP_PORT);
 
 
 // #define LED_COUNT 66
@@ -279,64 +286,64 @@ void modes_setup() {
 ##################################################### */
 
 void srv_handle_not_found() {
-  server.send(404, "text/plain", "File Not Found");
+  server1.send(404, "text/plain", "File Not Found");
 }
 
 void srv_handle_index_html() {
-  server.send_P(200,"text/html", index_html);
+  server1.send_P(200,"text/html", index_html);
 }
 
 void srv_handle_main_js() {
-  server.send_P(200,"application/javascript", main_js);
+  server1.send_P(200,"application/javascript", main_js);
 }
 
 void srv_handle_modes() {
-  server.send(200,"text/plain", modes);
+  server1.send(200,"text/plain", modes);
 }
 
 void srv_handle_set() {
-  for (uint8_t i=0; i < server.args(); i++){
-    if(server.argName(i) == "c") {
-      uint32_t tmp = (uint32_t) strtol(server.arg(i).c_str(), NULL, 10);
+  for (uint8_t i=0; i < server1.args(); i++){
+    if(server1.argName(i) == "c") {
+      uint32_t tmp = (uint32_t) strtol(server1.arg(i).c_str(), NULL, 10);
       if(tmp <= 0xFFFFFF) {
         ws2812fx.setColor(tmp);
       }
     }
 
-    if(server.argName(i) == "m") {
-      uint8_t tmp = (uint8_t) strtol(server.arg(i).c_str(), NULL, 10);
+    if(server1.argName(i) == "m") {
+      uint8_t tmp = (uint8_t) strtol(server1.arg(i).c_str(), NULL, 10);
       uint8_t new_mode = sizeof(myModes) > 0 ? myModes[tmp % sizeof(myModes)] : tmp % ws2812fx.getModeCount();
       ws2812fx.setMode(new_mode);
       auto_cycle = false;
       Serial.print("mode is "); Serial.println(ws2812fx.getModeName(ws2812fx.getMode()));
     }
 
-    if(server.argName(i) == "b") {
-      if(server.arg(i)[0] == '-') {
+    if(server1.argName(i) == "b") {
+      if(server1.arg(i)[0] == '-') {
         ws2812fx.setBrightness(ws2812fx.getBrightness() * 0.8);
-      } else if(server.arg(i)[0] == ' ') {
+      } else if(server1.arg(i)[0] == ' ') {
         ws2812fx.setBrightness(min(max(ws2812fx.getBrightness(), 5) * 1.2, 255));
       } else { // set brightness directly
-        uint8_t tmp = (uint8_t) strtol(server.arg(i).c_str(), NULL, 10);
+        uint8_t tmp = (uint8_t) strtol(server1.arg(i).c_str(), NULL, 10);
         ws2812fx.setBrightness(tmp);
       }
       Serial.print("brightness is "); Serial.println(ws2812fx.getBrightness());
     }
 
-    if(server.argName(i) == "s") {
-      if(server.arg(i)[0] == '-') {
+    if(server1.argName(i) == "s") {
+      if(server1.arg(i)[0] == '-') {
         ws2812fx.setSpeed(max(ws2812fx.getSpeed(), 5) * 1.2);
-      } else if(server.arg(i)[0] == ' ') {
+      } else if(server1.arg(i)[0] == ' ') {
         ws2812fx.setSpeed(ws2812fx.getSpeed() * 0.8);
       } else {
-        uint16_t tmp = (uint16_t) strtol(server.arg(i).c_str(), NULL, 10);
+        uint16_t tmp = (uint16_t) strtol(server1.arg(i).c_str(), NULL, 10);
         ws2812fx.setSpeed(tmp);
       }
       Serial.print("speed is "); Serial.println(ws2812fx.getSpeed());
     }
 
-    if(server.argName(i) == "a") {
-      if(server.arg(i)[0] == '-') {
+    if(server1.argName(i) == "a") {
+      if(server1.arg(i)[0] == '-') {
         auto_cycle = false;
       } else {
         auto_cycle = true;
@@ -344,43 +351,43 @@ void srv_handle_set() {
       }
     }
   }
-  server.send(200, "text/plain", "OK");
+  server1.send(200, "text/plain", "OK");
 }
 
-/*
- * Connect to WiFi. If no connection is made within WIFI_TIMEOUT, ESP gets resettet.
- */
-void wifi_setup() {
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(WIFI_SSID);
+// /*
+//  * Connect to WiFi. If no connection is made within WIFI_TIMEOUT, ESP gets resettet.
+//  */
+// void wifi_setup() {
+//   Serial.println();
+//   Serial.print("Connecting to ");
+//   Serial.println(WIFI_SSID);
 
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  WiFi.mode(WIFI_STA);
-  #ifdef STATIC_IP  
-    WiFi.config(ip, gateway, subnet);
-  #endif
+//   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+//   WiFi.mode(WIFI_STA);
+//   #ifdef STATIC_IP  
+//     WiFi.config(ip, gateway, subnet);
+//   #endif
 
-  unsigned long connect_start = millis();
-  while(WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+//   unsigned long connect_start = millis();
+//   while(WiFi.status() != WL_CONNECTED) {
+//     delay(500);
+//     Serial.print(".");
 
-    if(millis() - connect_start > WIFI_TIMEOUT) {
-      Serial.println();
-      Serial.print("Tried ");
-      Serial.print(WIFI_TIMEOUT);
-      Serial.print("ms. Resetting ESP now.");
-      ESP_RESET;
-    }
-  }
+//     if(millis() - connect_start > WIFI_TIMEOUT) {
+//       Serial.println();
+//       Serial.print("Tried ");
+//       Serial.print(WIFI_TIMEOUT);
+//       Serial.print("ms. Resetting ESP now.");
+//       ESP_RESET;
+//     }
+//   }
 
-  Serial.println("");
-  Serial.println("WiFi connected");  
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
-  Serial.println();
-}
+//   Serial.println("");
+//   Serial.println("WiFi connected");  
+//   Serial.print("IP address: ");
+//   Serial.println(WiFi.localIP());
+//   Serial.println();
+// }
 
 
 
@@ -389,8 +396,12 @@ void wifi_setup() {
 
 
 void setup() {
-  modes_setup();
+  Serial.begin(115200);                //波特率
+  LEDinit();                           //LED用于显示WiFi状态
 
+  connectToWiFi(connectTimeOut_s);     //连接wifi，传入的是wifi连接等待时间15s
+
+  modes_setup();
   Serial.println("WS2812FX setup");
   ws2812fx.init();
   ws2812fx.setMode(FX_MODE_STATIC);
@@ -400,15 +411,15 @@ void setup() {
   ws2812fx.start();
 
   Serial.println("Wifi setup");
-  wifi_setup();
+  //wifi_setup();
  
   Serial.println("HTTP server setup");
-  server.on("/", srv_handle_index_html);
-  server.on("/main.js", srv_handle_main_js);
-  server.on("/modes", srv_handle_modes);
-  server.on("/set", srv_handle_set);
-  server.onNotFound(srv_handle_not_found);
-  server.begin();
+  server1.on("/", srv_handle_index_html);
+  server1.on("/main.js", srv_handle_main_js);
+  server1.on("/modes", srv_handle_modes);
+  server1.on("/set", srv_handle_set);
+  server1.onNotFound(srv_handle_not_found);
+  server1.begin();
   Serial.println("HTTP server started.");
 
   Serial.println("ready!");
@@ -417,19 +428,34 @@ void setup() {
 void loop() {
   unsigned long now = millis();
 
-  server.handleClient();
-  ws2812fx.service();
-
-  if(now - last_wifi_check_time > WIFI_TIMEOUT) {
-    Serial.print("Checking WiFi... ");
-    if(WiFi.status() != WL_CONNECTED) {
-      Serial.println("WiFi connection lost. Reconnecting...");
-      wifi_setup();
-    } else {
-      Serial.println("OK");
+  if (!digitalRead(resetPin)) //长按5秒(P0)清除网络配置信息
+  {
+    delay(5000);              //哈哈哈哈，这样不准确
+    if (!digitalRead(resetPin)) 
+    {
+      Serial.println("\n按键已长按5秒,正在清空网络连保存接信息.");
+      restoreWiFi();     //删除保存的wifi信息
+      ESP.restart();     //重启复位esp32
+      Serial.println("已重启设备.");
     }
-    last_wifi_check_time = now;
   }
+
+  server1.handleClient();
+  ws2812fx.service();
+  
+  checkDNS_HTTP();                  //检测客户端DNS&HTTP请求，也就是检查配网页面那部分
+  checkConnect(true);               //检测网络连接状态，参数true表示如果断开重新连接
+
+  // if(now - last_wifi_check_time > WIFI_TIMEOUT) {
+  //   Serial.print("Checking WiFi... ");
+  //   if(WiFi.status() != WL_CONNECTED) {
+  //     Serial.println("WiFi connection lost. Reconnecting...");
+  //     wifi_setup();
+  //   } else {
+  //     Serial.println("OK");
+  //   }
+  //   last_wifi_check_time = now;
+  // }
 
   if(auto_cycle && (now - auto_last_change > 10000)) { // cycle effect mode every 10 seconds
     uint8_t next_mode = (ws2812fx.getMode() + 1) % ws2812fx.getModeCount();
